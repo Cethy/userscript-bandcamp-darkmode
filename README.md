042d0eaf1646be3e2b460b73c52db7eec0067c8a
Charles <cethy.hnc@gmail.com>
2025-05-29 13:19:31 +0100

    CHANGED: better wording

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Bandcamp.com Dark Mode (UserScript)
 
-Add switchable dark mode styling + auto-toggle if computer setup for preferred dark color scheme, but only on bandcamp collection pages !
+Add switchable dark mode styling + auto-toggle if computer setup for preferred dark color scheme, but only on bandcamp profile pages !
 
 | ![light](./docs/light.png) | ![dark](./docs/dark.png) |
 |:--------------------------:|:------------------------:|
@@ -9,6 +9,6 @@ Add switchable dark mode styling + auto-toggle if computer setup for preferred d
 # How to use
 
 - Install [Violentmonkey](https://violentmonkey.github.io/), greasemonkey, tampermonkey or any other userscript manager of your choice
-- Copy the `bandcamp.com_collection_DarkMode.js` script content
+- Copy the `bandcamp.com_profile_DarkMode.js` script content
 - Enable it on bandcamp.com
 - Enjoy!
--- bandcamp.com_profile_DarkMode.js
+++ bandcamp.com_profile_DarkMode.js
@@ -1,21 +1,21 @@
 // ==UserScript==
-// @name        bandcamp.com_collection_DarkMode
-// @namespace   cw.bandcamp.com_collection_DarkMode
+// @name        bandcamp.com_profile_DarkMode
+// @namespace   cw.bandcamp.com_profile_DarkMode
 // @match       https://bandcamp.com/*
 // @grant       none
 // @version     1.0
 // @author      Charles
 // @homepage    https://github.com/Cethy/userscript-bandcamp-darkmode
-// @description Add switchable dark mode styling + auto-toggle if computer setup for preferred dark color scheme, but only on bandcamp collection pages !
+// @description Add switchable dark mode styling + auto-toggle if computer setup for preferred dark color scheme, but only on bandcamp profile pages !
 // ==/UserScript==
 
 (function() {
     'use strict';
 
-    console.log("Hello, I'll set some dark mode styles and auto-toggle dark mode if it's your computer preferred color scheme, but only on collection pages !")
+    console.log("Hello, I'll set some dark mode styles and auto-toggle dark mode if it's your computer preferred color scheme, but only on profile pages !")
 
     const domParser = new DOMParser()
-    const darkModeSvgString = `<svg width="34px" height="34px" viewBox="0 0 24 24" version="1.1" xmlns="http://www.w3.org/2000/svg">
+    const darkModeSvgString = `<svg width="34px" height="34px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
       <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
           <g fill="#323232" fill-rule="nonzero">
               <path d="M12,22 C17.5228475,22 22,17.5228475 22,12 C22,6.4771525 17.5228475,2 12,2 C6.4771525,2 2,6.4771525 2,12 C2,17.5228475 6.4771525,22 12,22 Z M12,20.5 L12,3.5 C16.6944204,3.5 20.5,7.30557963 20.5,12 C20.5,16.6944204 16.6944204,20.5 12,20.5 Z" />
@@ -29,8 +29,8 @@
         return (style.display !== 'none') && (style.visibility !== 'hidden');
     }
 
-    // collection page detector
-    function isCollectionPage() {
+    // profile page detector
+    function isProfilePage() {
         return !!document.getElementById("fan-banner")
     }
 
@@ -52,7 +52,7 @@
     }
 
     function init() {
-        if(!isCollectionPage()) {
+        if(!isProfilePage()) {
             return;
         }
 
